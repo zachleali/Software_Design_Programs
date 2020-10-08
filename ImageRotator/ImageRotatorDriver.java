fb65b76383c71ccc4e0a540e6cb8021cea33dbdc
Zachary Leali <51714367+zachleali@users.noreply.github.com>
2020-10-08 11:07:40 -0500

    Create ImageRotatorDriver.java

---
+++ ImageRotator/ImageRotatorDriver.java
@@ -0,0 +1,20 @@
+// Zach Leali
+// Driver Program for ImageRotator
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ImageRotatorDriver
+{
+    public static void main(String[] args)
+    {
+        JFrame main = new JFrame();
+        S22_ImageRotator_Hard img = new S22_ImageRotator_Hard();
+        main.add(img);
+        main.getContentPane().add(BorderLayout.CENTER,img);
+        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main.setSize(600,600);
+        main.setVisible(true);
+    }
+
+}
