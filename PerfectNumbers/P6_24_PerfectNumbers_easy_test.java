fe7f361665d8cde360e407b097b584e9185a0400
Zachary Leali <51714367+zachleali@users.noreply.github.com>
2020-10-08 11:09:14 -0500

    Create P6_24_PerfectNumbers_easy_test.java

---
+++ PerfectNumbers/P6_24_PerfectNumbers_easy_test.java
@@ -0,0 +1,28 @@
+//Name: Zach Leali
+//Date: 9/11/2020
+
+import java.lang.*;
+import java.util.*;
+
+public class P6_24_PerfectNumbers_easy_test
+{
+    public static void main(String[] args)
+    {
+        P6_24_PerfectNumbers_easy test = new P6_24_PerfectNumbers_easy();
+        for(int i = 0; i < 1000; i++)
+        {
+            if(test.isPerfect(i))
+            {
+                if(i != 6)
+                {
+                    System.out.print(i + " = ");
+                    System.out.print(test.printFactors(i) + '\n');
+                }
+                else
+                {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
